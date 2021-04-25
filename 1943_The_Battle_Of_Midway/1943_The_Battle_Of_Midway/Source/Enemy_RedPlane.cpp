7feb3b7946f778924a8ab4d34ca35663f9cbcc82
ZaroDev <72089246+ZaroDev@users.noreply.github.com>
2021-04-25 16:40:53 +0200

    game implementation added

---
+++ 1943_The_Battle_Of_Midway/1943_The_Battle_Of_Midway/Source/Enemy_RedPlane.cpp
@@ -0,0 +1,36 @@
+#include "Enemy_RedPlane.h"
+
+#include "Application.h"
+#include "ModuleCollisions.h"
+
+Enemy_RedPlane::Enemy_RedPlane(int x, int y) : Enemy(x, y)
+{
+	flyAnim.PushBack({5,6,24,24});
+	flyAnim.PushBack({38, 6, 24, 24});
+	flyAnim.PushBack({71, 6, 24, 24});
+	flyAnim.PushBack({104, 6, 24, 24});
+	flyAnim.PushBack({137, 6, 24, 24});
+	flyAnim.PushBack({170, 6, 24, 24});
+	flyAnim.PushBack({203, 6, 24, 24});
+	flyAnim.PushBack({236, 6, 24, 24});
+	flyAnim.speed = 0.2f;
+
+	currentAnim = &flyAnim;
+
+	path.PushBack({ 0.0f, 1.0f }, 180, &flyAnim);
+	path.PushBack({ 0.0f, -1.0f }, 300, &flyAnim);
+	path.loop = false;
+
+	collider = App->collisions->AddCollider({0, 0, 24, 24}, Collider::Type::ENEMY, (Module*)App->enemies);
+}
+
+void Enemy_RedPlane::Update()
+{
+	path.Update();
+	position = spawnPos + path.GetRelativePosition();
+	currentAnim = path.GetCurrentAnimation();
+
+	// Call to the base class. It must be called at the end
+	// It will update the collider depending on the position
+	Enemy::Update();
+}
---
+++ 1943_The_Battle_Of_Midway/1943_The_Battle_Of_Midway/Source/ModuleScene.cpp
@@ -0,0 +1,55 @@
+#include "ModuleScene.h"
+
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleAudio.h"
+#include "ModuleCollisions.h"
+#include "ModuleEnemies.h"
+
+ModuleScene::ModuleScene()
+{
+
+}
+
+ModuleScene::~ModuleScene()
+{
+
+}
+
+// Load assets
+bool ModuleScene::Start()
+{
+	LOG("Loading background assets");
+
+	bool ret = true;
+
+	bgTexture = App->textures->Load("Assets/background.png");
+	App->audio->PlayMusic("Assets/stage1.ogg", 1.0f);
+
+	App->collisions->AddCollider({ 0, 0, 1000, 1000 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 240, 0, 1000, 1000 }, Collider::Type::WALL);
+	
+
+	// Enemies ---
+	App->enemies->AddEnemy(ENEMY_TYPE::REDPLANE, 200, -500);
+
+
+	return ret;
+}
+
+update_status ModuleScene::Update()
+{
+	App->render->camera.y -= 3;
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+// Update: draw background
+update_status ModuleScene::PostUpdate()
+{
+	// Draw everything --------------------------------------
+	App->render->Blit(bgTexture, 0, 0, NULL);
+
+	return update_status::UPDATE_CONTINUE;
+}
