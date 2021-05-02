2b584192de155876b0749288075b1d1076737336
ZaroDev <72089246+ZaroDev@users.noreply.github.com>
2021-05-02 22:21:28 +0200

    changed background

--- 1943_The_Battle_Of_Midway/1943_The_Battle_Of_Midway/Source/SceneLevel1.cpp
+++ 1943_The_Battle_Of_Midway/1943_The_Battle_Of_Midway/Source/SceneLevel1.cpp
@@ -25,18 +25,13 @@ bool SceneLevel1::Start()
 
 	bool ret = true;
 
-	bgTexture = App->textures->Load("Assets/Sprites/background.png");
-	App->audio->PlayMusic("Assets/Music/stage1.ogg", 1.0f);
+	bgTexture = App->textures->Load("Assets/SeaMap.png");
+	cloudTexture = App->textures->Load("Assets/CloudsMap.png");
+	App->audio->PlayMusic("Assets/stage1.ogg", 1.0f);
 
-	//Bottomside collider
-	App->collisions->AddCollider({ 0, 224, 3930, 16 }, Collider::Type::WALL);
-
-	//First two columns colliders
-	App->collisions->AddCollider({ 1375, 0, 111, 96 }, Collider::Type::WALL);
-	App->collisions->AddCollider({ 1375, 145, 111, 96 }, Collider::Type::WALL);
 
 	// Enemies ---
-	App->enemies->AddEnemy(ENEMY_TYPE::REDPLANE, 600, 80);
+	App->enemies->AddEnemy(ENEMY_TYPE::REDPLANE, 300, 80);
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
@@ -58,7 +53,8 @@ update_status SceneLevel1::Update()
 update_status SceneLevel1::PostUpdate()
 {
 	// Draw everything --------------------------------------
-	App->render->Blit(bgTexture, 0, 0, NULL);
+	App->render->Blit(cloudTexture, 0, -5600, NULL);
+	App->render->Blit(bgTexture, 0, -5600, NULL);
 
 	return update_status::UPDATE_CONTINUE;
 }
--- 1943_The_Battle_Of_Midway/1943_The_Battle_Of_Midway/Source/SceneLevel1.h
+++ 1943_The_Battle_Of_Midway/1943_The_Battle_Of_Midway/Source/SceneLevel1.h
@@ -34,6 +34,7 @@ public:
 	
 	// The scene sprite sheet loaded into an SDL_Texture
 	SDL_Texture* bgTexture = nullptr;
+	SDL_Texture* cloudTexture = nullptr;
 };
 
 #endif
